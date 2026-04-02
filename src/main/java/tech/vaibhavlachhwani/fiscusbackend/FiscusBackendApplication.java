c319e965482ba6f9f26db5c00451194d90f78c9c
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2026-04-02 21:40:34 +0530

    here be dragons

---
+++ src/main/java/tech/vaibhavlachhwani/fiscusbackend/FiscusBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.vaibhavlachhwani.fiscusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiscusBackendApplication.class, args);
+    }
+
+}
