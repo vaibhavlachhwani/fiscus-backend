f773f23bca5a4b7e6a69af68cd2c61844c72981b
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2026-04-03 14:58:51 +0530

    create dashboard controller

---
+++ src/main/java/tech/vaibhavlachhwani/fiscusbackend/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package tech.vaibhavlachhwani.fiscusbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.vaibhavlachhwani.fiscusbackend.dto.response.DashboardSummaryResponseDTO;
+import tech.vaibhavlachhwani.fiscusbackend.service.DashboardService;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @GetMapping("/metrics")
+    public ResponseEntity<DashboardSummaryResponseDTO> getSurvivalMetrics() {
+        DashboardSummaryResponseDTO metrics = dashboardService.getSurvivalMetrics();
+        return ResponseEntity.ok(metrics);
+    }
+}
